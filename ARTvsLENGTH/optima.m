7bc45c65d6abfede69635501779949cb74cb7af0
U-UEFAD\mgorbun <mgorbun@DNR6V44.uefad.uef.fi>
2026-01-30 15:31:07 +0200

    xxx

--- ARTvsLENGTH/optima.m
+++ ARTvsLENGTH/optima.m
@@ -1,88 +1,120 @@
-function reflt = optima(p, thcEXP)  
-    ptn = 1;
-    for jj = 1:length(thcEXP)
-        freqn = 2e12;
-        spl=299792458;
-        
-        nn1=1; nn2=1;
-        %Layer 1 Air
-        n01=1;          thcL1=1; 
-        
-        %Layer 2 Lorentz
-        thcL2 = thcEXP(jj);
+function refl = optima(p, thcEXP, nu0) 
 
-        %Layer 3 Drude
-        n03=1;          thcL3=2*1e-6;    tauL3=1e-19;   sig00L3=1*9e9*5e7; 
+    c = 29979245800; % speed of light in cm/s 
+    npt = 1;
+    nu = nu0;
+    omega = 2*pi*nu;  % angular frequency in rad/s
+    
+    refl = zeros(1, length(thcEXP));
+
+    for jj = 1:length(thcEXP)
+        Air = struct('type', 0, ...
+                    'd', 1, ...
+                    'n0', 1, ...
+                    'omega_p', 0, ...
+                    'tau', 0, ...
+                    'omega_0', 0);
+
+        Au = struct('type', 1, ...
+                'd', 2e-4, ...
+                'n0', 1, ...
+                'omega_p', sqrt(4*pi*9e9*5e7*1e19), ...
+                'tau', 1e-19);
+
+
+        Given = struct('type', -1, ...
+                     'd', thcEXP(jj)*1e2, ...
+                     'n0', 0, ...
+                     'eps1', p(1), ...
+                     'eps2', p(2), ...
+                     'omega', omega);
         
-        %Layer 4 Air
-        n04=1;          thcL4=1; 
+        structure = {Air, Given, Au, Air};
+        N = length(structure);
         
-        epsDi = zeros(1, ptn);
-        nDi = zeros(1, ptn);
-        deltaAu = zeros(1, ptn);
-        
-        for ii=1:ptn
-        
-          omega=2*pi*freqn(ii); kkk=omega/spl;
-          nL1 = n01;
-        
-          epsL2= p(1)+1j*p(2);
-          nL2=sqrt(epsL2);
-          epsDi(ii) = epsL2;
-          nDi(ii) = nL2;
-        
-          conL3=sig00L3/(1-1i*omega*tauL3); epsL3 = n03^2 + 4*pi*1i * conL3 ./ omega; nL3=sqrt(epsL3);
-          
-          nL4=n04;
-        
-          %1st boudary
-          nnl=nL4; nnr=nn2; aar=1; bbr=0;
-        
-          aal=(aar*(1+nnr/nnl)+bbr*(1-nnr/nnl))/2;
-          bbl=(aar*(1-nnr/nnl)+bbr*(1+nnr/nnl))/2;
-        
-        
-          %2nd boudary 
-          nnl=nL3; nnr=nL4; 
-        
-          aar=exp(-1i*kkk*nnr*thcL4)*aal;
-          bbr=exp(1i*kkk*nnr*thcL4)*bbl;
-          aal=(aar*(1+nnr/nnl)+bbr*(1-nnr/nnl))/2;
-          bbl=(aar*(1-nnr/nnl)+bbr*(1+nnr/nnl))/2;
-        
-        
-          %3rd boudary 
-          nnl=nL2; nnr=nL3; 
-        
-          aar=exp(-1i*kkk*nnr*thcL3)*aal;
-          bbr=exp(1i*kkk*nnr*thcL3)*bbl;
-        
-          deltaAu(ii) = kkk*nnr*thcL3;
-        
-          aal=(aar*(1+nnr/nnl)+bbr*(1-nnr/nnl))/2;
-          bbl=(aar*(1-nnr/nnl)+bbr*(1+nnr/nnl))/2;
-        
-          %4th boudary 
-          nnl=nL1; nnr=nL2;
-        
-          aar=exp(-1i*kkk*nnr*thcL2)*aal;
-          bbr=exp(1i*kkk*nnr*thcL2)*bbl;
-          aal=(aar*(1+nnr/nnl)+bbr*(1-nnr/nnl))/2;
-          bbl=(aar*(1-nnr/nnl)+bbr*(1+nnr/nnl))/2;
-        
-          %5th boudary 
-          nnl=nn1; nnr=nL1;
-        
-          aar=exp(-1i*kkk*nnr*thcL1)*aal;
-          bbr=exp(1i*kkk*nnr*thcL1)*bbl;
-          aal=(aar*(1+nnr/nnl)+bbr*(1-nnr/nnl))/2;
-          bbl=(aar*(1-nnr/nnl)+bbr*(1+nnr/nnl))/2;
-        
-          % trnsm(ii)=(abs(1/aal))^2;     
+        n_layers = zeros(N, npt);
+        eps_layers = zeros(N, npt);
+
+        for j = 1:N
+            curlayer = structure{j};
+            eps = curlayer.n0 ^ 2;
+            if curlayer.type == -1
+                eps = curlayer.eps1 + 1j*curlayer.eps2;
+            elseif curlayer.type == 1
+                w_p = curlayer.omega_p;
+                g   = 1 / curlayer.tau;
+                eps = eps - (w_p^2) ./ (omega.^2 + 1i*g.*omega);              % Drude model
+            elseif curlayer.type == 2
+                w_p = curlayer.omega_p;
+                for peak = 1:length(curlayer.omega_0)
+                    g   = 1 / curlayer.tau(peak);
+                    w_0 = curlayer.omega_0(peak);
+                    eps = eps + (w_p^2) ./ (w_0^2 - omega.^2 - 1i*g.*omega);  % Lorentz model
+                end
+            elseif curlayer.type == 3
+                w_p = curlayer.omega_p;
+                g   = 1 / curlayer.tauD;
+                eps = eps - (w_p^2) ./ (omega.^2 + 1i*g.*omega);
+                for peak = 1:length(curlayer.omega_0)
+                    g   = 1 / curlayer.tau(peak);
+                    w_0 = curlayer.omega_0(peak);
+                    eps = eps + (w_p^2) ./ (w_0^2 - omega.^2 - 1i*g.*omega);  % Drude-Lorentz model
+                end
+            end
+            % n_layers()
+            n_layers(j, :) = sqrt(eps);  % complex refractive index
+            eps_layers(j, :) = eps;  % complex 
+            % n_layers(j, :) = real(n_layers(j, :)) + 1i * abs(g(n_layers(j, :)));
         end
-        reflt(jj)=(abs(bbl/aal))^(2);   
-        % disp(reflt)
+        eps_layers(2) = p(1) + 1j*p(2);
+        n_layers(2) = sqrt(eps_layers(2));
+        
+        T = repmat(eye(2), 1, 1, npt);  % start with 2×2×N identity matrices
+        
+        for j = 2:N-1
+            curlayer = structure{j};
+            d = curlayer.d;
+            nL = n_layers(j-1, :);
+            nR = n_layers(j,   :);
+        
+            % Interface matrix M
+            M = zeros(2,2,npt);
+            M(1,1,:) = 1 + nL ./ nR;
+            M(1,2,:) = 1 - nL ./ nR;
+            M(2,1,:) = 1 - nL ./ nR;
+            M(2,2,:) = 1 + nL ./ nR;
+            M = 0.5 * M;
+            
+            % Multiply: T = T * B * P
+            T = pagemtimes(T, M);
+        
+            delta = omega .* nR .* d / c;  % 1×N
+        
+            % Propagation matrix P
+            P = zeros(2,2,npt);
+        
+            P(1,1,:) = exp(1i*delta);
+            P(2,2,:) = exp(-1i*delta);
+            
+            T = pagemtimes(T, P);
+        end
+        
+        % Final interface (N−1 to N)
+        nL = n_layers(N-1,:);
+        nR = n_layers(N,  :);
+        
+        M = zeros(2,2,npt);
+        M(1,1,:) = 1 + nL ./ nR;
+        M(1,2,:) = 1 - nL ./ nR;
+        M(2,1,:) = M(1,2,:);
+        M(2,2,:) = M(1,1,:);
+        M = 0.5 * M;
+        
+        T = pagemtimes(T, M);  % final multiply
+        
+        Ein = squeeze(T(:,2,:));  % incoming field from right-side excitation
+        
+        refl(jj) = abs(Ein(1,:) ./ Ein(2,:)).^2;
+        % trans = abs(1 ./ Ein(2,:)).^2;
     end
-    % figure
-    % plot(thcEXP, reflt, 'b');
 end
